1ba08b0b374af2bdea35834fa72bed08d3afe966
Cameron Anderson <cammando@hotmail.com.au>
2025-12-25 20:01:35 +1100

    Update login landing screen to allow access to user signup for new users

--- gym-tracker/app/(auth)/login.tsx
+++ gym-tracker/app/(auth)/login.tsx
@@ -1,5 +1,16 @@
 import LogonForm from "@/forms/Logon";
+import { View, Pressable, Text } from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function Login() {
-    return <LogonForm></LogonForm>
+    const router = useRouter();
+
+    return (
+        <View>
+            <LogonForm></LogonForm>
+            <Pressable onPress={() => router.navigate("/(auth)/signup")}>
+                <Text>Signup</Text>
+            </Pressable>
+        </View>
+    )
 }
---
+++ gym-tracker/app/(auth)/signup.tsx
@@ -0,0 +1,11 @@
+import AccountForm from '@/forms/AccountForm';
+import { Text, View } from 'react-native';
+
+export default function Account() {
+    return (
+        <View>
+            <Text>Account</Text>
+            <AccountForm></AccountForm>
+        </View>
+    )
+}
