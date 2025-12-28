d5fdd40438bf7460ac1eb3aceca59de260837c15
Cameron Anderson <cammando@hotmail.com.au>
2025-12-29 09:59:16 +1100

    Update signup endpoint to store user PRs in users_prs instead of users table

--- gym-backend/routes/auth.js
+++ gym-backend/routes/auth.js
@@ -11,26 +11,43 @@ router.post('/signup', async (req, res) => {
     try {
         console.log('Received signup request with details: ', req.body);
         //Get values from submitted request
-        const { name, birthDay, birthMonth, birthYear, username, weight, weightUnit, benchPr, squatPr, deadPr, password } = req.body;
-        if (!name || !birthDay || !birthMonth || !birthYear || !username || !weightUnit || !password) {
+        const { name, birthday, username, weight, weightUnit, benchPr, squatPr, deadPr, password } = req.body;
+        if (!name || !birthday || !username || !weightUnit || !password) {
             throw new Error('Missing required fields');
         }
         //Create hash of password for db
-        console.log('creating hash');
         const pwHash = await bcrypt.hash(password, 10);
-        console.log('hash created')
-        const creationDate = new Date();
-        const birthdate = new Date(birthYear, birthMonth, birthDay);
+
+        //Build array of provided PRs
+        const benchDbId = '019ef03e-347f-4b1c-b58b-1ab9b6201b92'
+        const deadDbId = '6ebc0990-4883-4f62-92b9-c2982e3f1bd1'
+        const squatDbId = '8b24892e-6d79-4e0c-9e17-641530a4844c'
+        const prList = [];
+        if (benchPr > 0) prList.push({exerciseId: benchDbId, weight: benchPr});
+        if (deadPr > 0) prList.push({exerciseId: deadDbId, weight: deadPr});
+        if (squatPr > 0) prList.push({exerciseId: squatDbId, weight: squatPr});
 
         try {
             console.log('Attempting to insert user...');
-            await pool.query(
-                `INSERT INTO users
-                (name, username, birthdate, created_at, weight, weight_unit, pr_bench, pr_deadlift, pr_squat, password)
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
-                [name, username, birthdate, creationDate, weight, weightUnit, benchPr, deadPr, squatPr, pwHash]
+            const newUser = await pool.query(
+                `INSERT INTO users (name, username, birthdate, created_at, weight, weight_unit, password)
+                VALUES ($1, $2, $3, $4, $5, $6, $7)
+                RETURNING id`,
+                [name.toLowerCase(), username.toLowerCase(), birthday, new Date(), weight, weightUnit, pwHash]
             )
             console.log('User successfully added to DB');
+
+            const newUserId = newUser.rows[0].id;
+
+            console.log('Attempting to add PRs to DB...');
+            prList.map(async pr => {
+                await pool.query(
+                    `INSERT INTO users_prs (exercise_id, user_id, last_set, weight)
+                    VALUES ($1, $2, $3, $4)`,
+                    [pr.exerciseId, newUserId, new Date(), pr.weight]
+                );
+            })
+            console.log('PRs added to DB');
         } catch (error) {
             console.log('DB insert error:', error);
             return res.status(400).json({error: error.message});
