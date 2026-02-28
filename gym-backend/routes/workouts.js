7cea5024fb1338a74ef003775354c905909f7719
Cameron Anderson <cammando@hotmail.com.au>
2026-02-28 18:42:11 +1100

    Refactor create-workout endpoint into a db transaction with rollback when required

--- gym-backend/routes/workouts.js
+++ gym-backend/routes/workouts.js
@@ -6,82 +6,98 @@ dotenv.config();
 const router = express.Router();
 
 router.post('/templates/create', async(req, res) => {
-    try {
-        console.log('Received new workout with details: ', req.body);
-        const { username, workoutName, privacy } = req.body;
+    const client = await pool.connect();
 
+    try {
+        await client.query('BEGIN');
+
+        console.log('Received new workout template');
+        const { username, workoutName, privacy } = req.body;
         const dateCreated = new Date();
 
-        //Check user submitting exists
-        const usernameQuery = await pool.query('SELECT id FROM users WHERE username = $1', [username]);
-        const user = usernameQuery.rows[0].id;
-        if (!user) return res.status(401).json({error: 'User not found'});
+        //Check username submitting request exists in DB:
+        const usernameQuery = await client.query('SELECT id FROM users WHERE username = $1', [username]);
+        if (usernameQuery.rows.length === 0) throw new Error('Username not found');
+        const userId = usernameQuery.rows[0].id;
 
-        //Create workout template in DB
-        console.log('Attempting to create workout template...');
-        const insertNewWorkout = await pool.query(
-            `INSERT INTO workout_templates (created_at, user_id, workout_name, privacy)
+        //Create workout template
+        console.log('Creating new workout template...')
+        const insertWorkoutTemplate = await client.query(
+            `INSERT INTO workout_templates
+            (created_at, user_id, workout_name, privacy)
             VALUES ($1, $2, $3, $4)
             RETURNING id`,
-            [dateCreated, user, workoutName, privacy]
+            [dateCreated, userId, workoutName, privacy]
         );
-        const newWorkoutID = insertNewWorkout.rows[0].id;
-        console.log('Workout template created');
+        //Get ID of newly created workout template to link to exercises on template
+        const workoutTemplateId = insertWorkoutTemplate.rows[0].id;
+        console.log('New workout template created.')
 
-        //Add exercises to workout template
-        console.log('Attempting to create workout exercises');
-
-        //Get set types
-        const dbSetTypes = await pool.query(`SELECT id, name FROM set_types`);
+        //Get set type IDs from DB
+        const dbSetTypes = await client.query('SELECT id, name FROM set_types');
         const setTypesMap = new Map(dbSetTypes.rows.map(row => [row.name, row.id]));
 
-        req.body.exercises.map(async (exc, i) => {
-            let newExerciseId;
-            if(exc.dbId === '') {
-                const createNewExercise = await pool.query(
+        //Insert each exercise in workout
+        for (const [exerciseIndex, exc] of req.body.exercises.entries()) {
+            //Exercises created by the user will have '' as the DB id. Add these to exercises table before adding workout exercises
+            let exerciseId = exc.dbId;
+
+            if(exerciseId === '') {
+                console.log('Adding new exercise to DB...');
+                const createNewExercise = await client.query(
                     `INSERT INTO exercises
                     (created_at, name, target_muscle, is_unilateral, opt_set_mod_unilateral, opt_set_mod_straps, opt_set_mod_belt)
                     VALUES ($1, $2, $3, $4, $5, $6, $7)
                     RETURNING id`,
                     [dateCreated, exc.exerciseName, exc.targetMuscle, exc.unilateralExercise, exc.setOptionalUnilateral, exc.setOptionalStraps, exc.setOptionalBelt] 
                 );
-                newExerciseId = createNewExercise.rows[0].id;
+                exerciseId = createNewExercise.rows[0].id;
+                console.log('New exercise created in DB.');
             }
-            const newTemplateExercise = await pool.query(
+
+            //Insert exercise into template exercises table
+            console.log('Adding new exercise to workout template...');
+            const newTemplateExercise = await client.query(
                 `INSERT INTO workout_template_exercises
                 (workout_template_id, exercise_index, exercise_id, rep_range_lower, rep_range_upper)
                 VALUES ($1, $2, $3, $4, $5)
                 RETURNING id`,
-                [newWorkoutID, i, newExerciseId ? newExerciseId : exc.dbId, exc.repRangeLower, exc.repRangeHigher]
+                [workoutTemplateId, exerciseIndex, exerciseId, exc.repRangeLower, exc.repRangeHigher]
             );
-            const templateExerciseId = newTemplateExercise.rows[0].id;
+            const newExerciseId = newTemplateExercise.rows[0].id;
 
-            //Add template sets per exericse
-            console.log(`Attempting to add exercise index ${i} sets`);
-            console.log(exc.sets);
-            exc.sets.map(async (s, ind) => {
-                if (!setTypesMap.has(s.setType)) throw new Error(`Set Type: ${s.setType} not found`);
-                await pool.query(
-                    `INSERT INTO workout_template_exercise_sets
-                    (workout_template_exercise_id, set_index, set_type, is_unilateral)
-                    VALUES ($1, $2, $3, $4)`,
-                    [templateExerciseId, ind, setTypesMap.get(s.setType), s.unilateralSet]
-                )
+            console.log('Adding sets to exercise in workout template...');
+            const values = [];
+            const placeholders = [];
+
+            //Build query values and placeholders from set array
+            exc.sets.forEach((s, sIndex) => {
+                if (!setTypesMap.has(s.setType)) throw new Error(`Invalid set type: ${s.setType}`);
+
+                const baseIndex = values.length;
+
+                values.push(newExerciseId, sIndex, setTypesMap.get(s.setType), s.unilateralSet);
+                placeholders.push(`($${baseIndex + 1}, $${baseIndex + 2}, $${baseIndex + 3}, $${baseIndex + 4})`);
             });
-            console.log(`Sets added for exercise index ${i}`)
 
-        });
-        console.log('Exercises Added');
+            await client.query(
+                `INSERT INTO workout_template_exercise_sets
+                (workout_template_exercise_id, set_index, set_type, is_unilateral)
+                VALUES ${placeholders.join(',')}`,
+                values
+            );
+            console.log('Sets added.');
+            console.log('Exercise added.');
+        }
 
-        //Add sets for each workout exercise
-
-        return res.status(201).json({
-            message: 'Workout Created'
-        })
-
+        await client.query('COMMIT');
+        return res.status(201).json({ message: 'Workout Template Created' });
     } catch (error) {
+        await client.query('ROLLBACK');
         console.error(error);
         return res.status(500).json({ error: error.message });
+    } finally {
+        client.release();
     }
 })
 
