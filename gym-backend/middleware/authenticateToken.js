9f9fa0f9dfbcfbb861ac16cb8dfdfc224e6264fb
Cameron Anderson <cammando@hotmail.com.au>
2026-03-13 22:42:06 +1100

    Add middleware to authenticate user when receiving API requests

---
+++ gym-backend/middleware/authenticateToken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.sendStatus(403);
+        req.user = decoded;
+        next();
+    })
+}
